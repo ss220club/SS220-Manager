844bcad637d4e37ca6e1383eaa79de8f771ebbbb
Legendaxe <102746941+Legendaxe@users.noreply.github.com>
2023-11-27 17:39:08 +0200

    add Black server

--- src/helpers.py
+++ src/helpers.py
@@ -17,8 +17,9 @@ DEPARTMENT_TRANSLATIONS = {"├Призраком": "Ghost",
                            }
 
 SERVERS_NICE = {"136.243.82.223:4002": ["Main", "https://cdn.discordapp.com/emojis/1098305756836663379.webp?size=64"],
-                "141.95.72.94:4002": ["Main", "https://cdn.discordapp.com/emojis/1098305756836663379.webp?size=64"],
-                "135.125.189.154:4001": ["Prime", "https://cdn.discordapp.com/emojis/1100109697744371852.webp?size=64"]
+                "141.95.72.94:4002": ["Green", "https://cdn.discordapp.com/emojis/1098305756836663379.webp?size=64"],
+                "135.125.189.154:4001": ["Prime", "https://cdn.discordapp.com/emojis/1100109697744371852.webp?size=64"],
+                "135.125.189.154:4000": ["Black", "https://cdn.discordapp.com/emojis/1098305756836663379.webp?size=64"]
                 }  # TODO: To config
 
 
