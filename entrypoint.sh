bc2e107628854313c22e59e78fc606511775de7e
Mikhail Dzianishchyts <mikhail.dzianishchyts@gmail.com>
2025-08-07 18:15:19 +0300

    wip: bring old entrypoint.sh back

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CURRENT=$(alembic current)
+HEADS=$(alembic heads)
+
+if [ "$CURRENT" = "$HEADS" ]; then
+  echo "Database is up to date"
+else
+  echo "Database is not up to date ($CURRENT != $HEADS), run alembic upgrade head"
+  exit 1
+fi
+
+exec "$@"
